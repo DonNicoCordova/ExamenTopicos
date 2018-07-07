745b8a6165e5b6f3a934a7f209772071c7854e99
Nicolas Cordova <n.cordovapeaylillo@uandresbello.edu>
2018-07-07 19:10:07 -0400

    agregado navbar y footer

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Index(request):
+    data = {}
+    template_name = 'Index.html'
+    return render(request,template_name,data) 
--- examen/urls.py
+++ examen/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from core.views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index,name="index"),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
