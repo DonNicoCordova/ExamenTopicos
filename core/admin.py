537afa70add91873822877a3bbe530d0330e288c
Nicolas Cordova <n.cordovapeaylillo@uandresbello.edu>
2018-07-08 17:20:51 -0400

    agregado contacto de prueba, y arreglado footer para que se comporte como es solicitado

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Contacto
+from django.utils.safestring import mark_safe
 
 # Register your models here.
+@admin.register(Contacto)
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'email', 'direccion', 'pk', 'thumb' )
+
+    def thumb(self, obj):
+        return mark_safe(u'<img src="%s" style="width:10px;height:10px;"/>' \
+            % (obj.imagen.url))
